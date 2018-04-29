1bf0725e50f25cb706406104ab00b03b12640618
dorongal1 <dorongal1@gmail.com>
2018-04-29 07:55:28 +0300

    l;'k
    lk;'

---
+++ elasticsearch/elasticsearch/Controllers/HomeController.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using elasticsearch.Models;
+using Nest;
+using System.Data;
+using System.Dynamic;
+
+namespace elasticsearch.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            var node = new Uri("http://localhost:9200");
+            var settings = new ConnectionSettings(node);
+            var client = new ElasticClient(settings);
+
+
+
+            var table = new DataTable();
+
+            table.Columns.Add("Dosage", typeof(int));
+            table.Columns.Add("Drug", typeof(string));
+            table.Columns.Add("Patient", typeof(string));
+            table.Columns.Add("Date", typeof(DateTime));
+
+            // Here we add five DataRows.
+            table.Rows.Add(25, "Indocin", "David", DateTime.Now);
+            table.Rows.Add(50, "Enebrel", "Sam", DateTime.Now);
+            table.Rows.Add(10, "Hydralazine", "Christoff", DateTime.Now);
+            table.Rows.Add(21, "Combivent", "Janet", DateTime.Now);
+            table.Rows.Add(100, "Dilantin", "Melanie", DateTime.Now);
+
+
+            var dicty = new skrtt();
+
+            foreach (DataRow row in table.Rows)
+            {
+                foreach (DataColumn column in table.Columns)
+                {
+                    var asdf = column.ToString();
+                    dicty.GetType().GetProperty(asdf).SetValue(dicty, row[column]);
+                }
+                var response = client.Index(dicty, idx => idx.Index("456456456")); //or specify index via settings.DefaultIndex("mytweetindex");
+                dicty = new skrtt();
+
+                var safd = 123;
+            }
+
+            dynamic expando = new ExpandoObject();
+            expando.Name = "Brian";
+            expando.Country = "USA";
+            AddProperty(expando, "Language", "English");
+
+
+            return View();
+        }
+
+        public IActionResult About()
+        {
+            ViewData["Message"] = "Your application description page.";
+
+            return View();
+        }
+
+        public IActionResult Contact()
+        {
+            ViewData["Message"] = "Your contact page.";
+
+            return View();
+        }
+
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ elasticsearch/elasticsearch/Models/skrtt.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace elasticsearch.Models
+{
+    public class skrtt
+    {
+
+        public int Dosage { get; set; }
+        public string Drug { get; set; }
+        public string Patient { get; set; }
+        public DateTime Date { get; set; }
+    }
+}
